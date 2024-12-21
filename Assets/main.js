0fea2b63c36c15e4bbfe47ec2491a6dcaabce494
Smaeil Ahmadi <smaeilahmadi@Smaeils-MacBook-Pro.local>
2024-12-21 22:22:18 +0100

    paging function was done

--- Assets/main.js
+++ Assets/main.js
@@ -1,13 +1,18 @@
-// Global Variables: ********************************************
+// Global Variables and objects: ********************************************
 
 const navDropDown = document.querySelector('#nav-dropdown');
+const pagesCollection = document.querySelectorAll('.page')
+const pages = Array.from(pagesCollection);
+let currentPageIndex = 0;
+
+
 
 
 
 
 // interface control: *******************************************
 
-// nav
+// nav menu
 function toggleDropdown() {
     if (navDropDown.classList.contains('flex')) {
         navDropDown.classList.remove('flex');
@@ -16,9 +21,90 @@ function toggleDropdown() {
     }
 }
 
+// Dark mode:
+
+// Language Change:
+
+
+    // recalling the lang from local storage:
+
+// navigation: *************************************************
+
+async function getPage(pageId) {
+    try {
+        const targetPage = document.querySelector(`#${pageId}`);
+        const targetPageIndex = pages.indexOf(targetPage);
+        
+        // finding the navigation direction:
+        const isForward = targetPageIndex - currentPageIndex > 0 ? true : false;
+        // finding the number of steps:
+        const steps = Math.abs(targetPageIndex - currentPageIndex);
+        
+        if (steps === 0) return;
+        
+        if(isForward) {
+            for(let i = 0; i < steps; i++) {
+                // setting the currentpage and next page
+                const currentPage = pagesCollection[currentPageIndex];
+                const nextPage = pagesCollection[currentPageIndex + 1];
+    
+                nextPage.classList.add('top');
+                nextPage.classList.add('cover');
+                nextPage.classList.remove('hide');
+    
+                await delay(401);
+                currentPage.classList.add('hide');
+                nextPage.classList.remove('cover');
+                nextPage.classList.remove('top');
+    
+                // setting the new current page index
+                currentPageIndex++;
+
+                // updating scroll spy
+                document.querySelector('#scroll-spy').style.width = `${currentPageIndex * 25}%`;
+
+                // updating the history ???
+            }
+        } else {
+            for(let i = 0; i < steps; i++) {
+                // setting the currentpage and next page
+                const currentPage = pagesCollection[currentPageIndex];
+                const nextPage = pagesCollection[currentPageIndex - 1];
+    
+                nextPage.classList.add('top');
+                nextPage.classList.add('uncover');
+                nextPage.classList.remove('hide');
+    
+                await delay(401);
+                currentPage.classList.add('hide');
+                nextPage.classList.remove('uncover');
+                nextPage.classList.remove('top');
+    
+                // setting the new current page index
+                currentPageIndex--;
+
+                // updating scroll spy
+                document.querySelector('#scroll-spy').style.width = `${currentPageIndex * 25}%`;
+
+                // updating the history ???
+            }
+    
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
 // Page contents: ***********************************************
 
+// core functions: *********************************************
 
+function delay(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
 // App Listeners: ***********************************************
 
 // closes the menus on click:
@@ -29,3 +115,15 @@ window.addEventListener('click', e => {
         navDropDown.classList.remove('flex');
     }
 });
+
+
+
+// window.addEventListener('scroll', () => {
+
+//     // scroll spy: ***************************
+//     const scrollSpy = document.querySelector('#scroll-spy');
+//     const scrollPercentage = Math.round((document.documentElement.scrollTop) / (document.documentElement.scrollHeight - window.innerHeight) * 100);
+
+//     scrollSpy.style.width = `${scrollPercentage}%`;
+
+// });
